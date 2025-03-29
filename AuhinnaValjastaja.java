e75c247552f373263bceb72382273d65963562ed
georgkle <georgkletu@gmail.com>
2025-03-29 23:38:47 +0200

    Add files via upload

---
+++ AuhinnaValjastaja.java
@@ -0,0 +1,35 @@
+package ProjektVideoPoker;
+
+import java.util.List;
+
+public class AuhinnaValjastaja {
+    private List<Kaart> kaardipakk;
+
+    public AuhinnaValjastaja(List<Kaart> kaardipakk) {
+        this.kaardipakk = kaardipakk;
+    }
+
+    public double arvutatasu(double panus, String tulemus) {
+
+        return switch (tulemus) {
+            case "Royal Flush" -> 250 * panus;
+            case "Straight Flush" -> 50 * panus;
+            case "Straight" -> 4 * panus;
+            case "One Pair" -> panus; // Lisa vastav väärtus või loogika
+            case "Two Pair" -> 2*panus;
+            case "High Card" -> 0;
+            case "Flush" -> 6*panus;
+            case "Three of a Kind" -> 3*panus;
+            case "Four of a Kind" -> 25*panus;
+            case "Full House" -> 9*panus;
+
+
+            default -> 0; // Kui pole vastet, tagasta 0
+        };
+}}
+
+
+
+
+
+
